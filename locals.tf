d5f2c375343e5ea3f7f7cfe396638c745ca06405
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2021-12-29 10:28:20 +0000

    PBD-2401 Safer method for writting values from secrets
    Previous method seemed to result in yq errors if the secret contained certain special characters.

--- locals.tf
+++ locals.tf
@@ -24,12 +24,12 @@ apk add yq
 xray_system_yaml_path="/mnt/xray-persistent-volume/etc/system.yaml"
 mkdir -p $(dirname $${xray_system_yaml_path})
 yq eval -i '.shared.jfrogUrl = "${var.artifactory_url}"' $${xray_system_yaml_path}
-artifactory_join_key=$${ARTIFACTORY_JOIN_KEY} yq eval -i '.shared.security.joinKey = env(artifactory_join_key)' $${xray_system_yaml_path}
+yq eval -i '.shared.security.joinKey = "'"$(echo $${ARTIFACTORY_JOIN_KEY})"'"' $${xray_system_yaml_path}
 yq eval -i '.shared.database.type = "postgresql"' $${xray_system_yaml_path}
 yq eval -i '.shared.database.driver = "rg.postgresql.Driver"' $${xray_system_yaml_path}
 yq eval -i '.shared.database.url = "postgres://${aws_db_instance.main.endpoint}/jfrogxray?sslmode=disable"' $${xray_system_yaml_path}
 yq eval -i '.shared.database.username = "jfrogxray"' $${xray_system_yaml_path}
-rds_password=$${RDS_PASSWORD} yq eval -i '.shared.database.password = env(rds_password)' $${xray_system_yaml_path}
+yq eval -i '.shared.database.password = "'"$(echo $${RDS_PASSWORD})"'"' $${xray_system_yaml_path}
 
 curl -LO https://releases.jfrog.io/artifactory/jfrog-xray/xray-compose/${var.xray_version}/jfrog-xray-${var.xray_version}-compose.tar.gz
 tar -zxf jfrog-xray-${var.xray_version}-compose.tar.gz
@@ -40,8 +40,9 @@ echo "[rabbitmq_management,rabbitmq_prometheus]." > /mnt/rabbitmq-persistent-vol
 
 chown -R ${local.xray_uid}:${local.xray_uid} /mnt/xray-persistent-volume
 chown -R ${local.rabbitmq_uid}:${local.rabbitmq_uid} /mnt/rabbitmq-persistent-volume
-echo "****DONE****"
+
 sleep 30s
+echo "****DONE****"
 EOT
 }
 
