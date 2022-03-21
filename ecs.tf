88f0ed904d3547699e9d078ae7b2fd557c22b519
Steve Reddel <1107842+sreddel@users.noreply.github.com>
2022-03-21 10:31:01 +0000

    WIP

--- ecs.tf
+++ ecs.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_service" "main" {
   name                               = "xray"
   cluster                            = aws_ecs_cluster.main.arn
   task_definition                    = aws_ecs_task_definition.main.arn
-  desired_count                      = 1
+  desired_count                      = 0
   deployment_maximum_percent         = 200
   deployment_minimum_healthy_percent = 100
   launch_type                        = "FARGATE"
